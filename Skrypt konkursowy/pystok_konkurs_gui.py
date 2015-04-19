1a6230477a1aaf0981381aacf4e4ff91b247f2cb
Paweł Jan Czochański <czochanski@gmail.com>
2015-04-19 16:43:30 +0200

    New GUI winner generator.

---
+++ Skrypt konkursowy/pystok_konkurs_gui.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import Tkinter as tk
+import locale
+import random
+
+
+fontsize = 100
+
+participants = [
+    # Lista uczestników zarejestrowanych
+]
+
+
+class WinnerGUI(object):
+
+    def __init__(self, fontsize):
+        self.root = tk.Tk()
+        self.root.attributes('-zoomed', True)
+        self.stop = False
+        self.fontsize = fontsize
+        self.bind_events()
+
+    def bind_events(self):
+        self.root.bind_all('<Key>', self.keypress)
+        self.root.bind('<Button-1>', self.keypress)
+
+    def keypress(self, key):
+        self.stop = True
+
+    def show(self):
+        the_chosen_one = tk.StringVar()
+        label = tk.Label(self.root, textvariable=the_chosen_one,
+                         font=(None, self.fontsize))
+        label.configure(background='white')
+        label.pack(expand=tk.YES, fill=tk.BOTH)
+        self.randomize_winner(the_chosen_one)
+        self.root.mainloop()
+
+    def randomize_winner(self, the_chosen_one):
+        the_chosen_one.set('The winner is: {}'.format(
+            random.choice(participants).encode('utf-8')))
+
+        if not self.stop:
+            self.root.after(5, self.randomize_winner, the_chosen_one)
+
+
+if __name__ == '__main__':
+    if not participants:
+        print 'No participants provided. Finishing.'
+    else:
+        locale.setlocale(locale.LC_ALL, "")
+        window = WinnerGUI(fontsize)
+        window.show()
